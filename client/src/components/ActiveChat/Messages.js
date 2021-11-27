c7f5642a230a12752287d08bebf77e4b7ff75c1e
kasdeya <weirdusername@gmail.com>
2021-11-26 21:18:01 -0600

    Add read indicator to Messages component

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,20 +1,54 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
+
+import { SenderBubble, OtherUserBubble } from "../ActiveChat";
+import ReadIndicator from "./ReadIndicator";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+
+  // we need to know what the newest consecutive read message is, so that we
+  // can put the ReadIndicator just after it.
+  let newestConsecutiveReadMessage;
+  for (const message of messages) {
+    if (message.senderId !== userId) {
+      continue;
+    }
+
+    if (!message.readByRecipient) {
+      break;
+    }
+
+    newestConsecutiveReadMessage = message;
+  }
 
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
-        return message.senderId === userId ? (
+        const bubble = message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+        );
+
+        const readIndicator = message.id === newestConsecutiveReadMessage?.id ? (
+          <ReadIndicator
+            key="ReadIndicator"
+            username={otherUser.username}
+            photoUrl={otherUser.photoUrl}
+          />
+        ) : (
+          null
+        );
+
+        return (
+          <>
+            {bubble}
+            {readIndicator}
+          </>
         );
       })}
     </Box>
