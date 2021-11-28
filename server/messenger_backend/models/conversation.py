71cfd562914fe64b3d1b027362b9cb0088fb2e18
kasdeya <weirdusername@gmail.com>
2021-11-27 23:12:33 -0600

    Modify Conversation model to accept any number of users.

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -7,22 +7,10 @@ from .user import User
 
 class Conversation(utils.CustomModel):
 
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
+    users = models.ManyToManyField(User, related_name="+")
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
-        # return conversation or None if it doesn't exist
-        try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
-        except Conversation.DoesNotExist:
-            return None
+    # find_conversation has been removed because of the change from `user1` and
+    # `user2` to `users`. many-to-many fields can't be unique, which means that
+    # there could be more than one conversation for a given set of user IDs.
