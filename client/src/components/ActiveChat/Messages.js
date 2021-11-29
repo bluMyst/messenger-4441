9253161e778f8636636df6c82f966e809b04a84a
kasdeya <weirdusername@gmail.com>
2021-11-28 18:50:05 -0600

    Rewrite SenderBubble to have hasReadIndicator prop
    Now the ReadIndicator isn't added by Messages.js, but by SenderBubble itself when hasReadIndicator is true.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -3,7 +3,6 @@ import { Box } from "@material-ui/core";
 import moment from "moment";
 
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
-import ReadIndicator from "./ReadIndicator";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
@@ -28,27 +27,16 @@ const Messages = (props) => {
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
-        const bubble = message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
-
-        const readIndicator = message.id === newestConsecutiveReadMessage?.id ? (
-          <ReadIndicator
-            key="ReadIndicator"
-            username={otherUser.username}
-            photoUrl={otherUser.photoUrl}
+        return message.senderId === userId ? (
+          <SenderBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+            hasReadIndicator={message.id === newestConsecutiveReadMessage?.id}
           />
         ) : (
-          null
-        );
-
-        return (
-          <>
-            {bubble}
-            {readIndicator}
-          </>
+          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
       })}
     </Box>
